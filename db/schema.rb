e30ad0c886eab12a19e7e931998b2f0cdca63214
Jonathan Hong <jonhong777@gmail.com>
2018-08-08 03:01:37 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "costumes", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.string "size"
+    t.string "imageurl"
+  end
 
 end
